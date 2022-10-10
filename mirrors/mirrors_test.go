0b31ea55a28679f0beece789fdef14f2e8223c99
thanos <koutroubas.thanos@gmail.com>
2022-10-10 23:23:09 +0300

    Add mirror unit tests

--- mirrors/mirrors_test.go
+++ mirrors/mirrors_test.go
@@ -1,8 +1,10 @@
 package mirrors
 
 import (
+	"math"
 	"net/url"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -38,7 +40,26 @@ func TestReadMirrorsJSON(t *testing.T) {
 			URL:         &url.URL{Scheme: "https", Host: "mirror.ubuntu.ikoula.com", Path: "/"},
 			Protocol:    443,
 		},
+		{
+			Country:     "South Africa",
+			CountryCode: "ZA",
+			URL:         &url.URL{Scheme: "ftp", Host: "mirror.wiru.co.za", Path: "/ubuntu/"},
+			Protocol:    21,
+		},
 	}
 	actual := ReadMirrorsJSON(input)
 	assert.Equal(t, expected, actual)
+}
+
+func TestGetTime(t *testing.T) {
+	input := ReadMirrorsJSON("../inputs/in.template.json")
+	expected := time.Duration(math.MaxInt64)
+	actual := time.Duration(0)
+
+	for _, m := range input {
+		actual = m.GetTime()
+		if m.URL.Scheme != "http" && m.URL.Scheme != "https" {
+			assert.Equal(t, expected, actual)
+		}
+	}
 }
